3ebeb6d6f2a049b75fd8ad71f68b6bca0f98f60f
stharavi01 <sanayashe@gmail.com>
2024-03-25 16:12:05 +0545

    learning http

--- 01-node-tutorial/12-http.js
+++ 01-node-tutorial/12-http.js
@@ -1,17 +1,17 @@
-const http = require('http')
+const http = require('http');
 
 const server = http.createServer((req, res) => {
-  //   if (req.url === '/') {
-  //     res.end('Welcome to our home page')
-  //   }
-  //   if (req.url === '/about') {
-  //     res.end('Here is our short history')
-  //   }
-  //   res.end(`
-  //   <h1>Oops!</h1>
-  // <p>We can't seem to find the page you are looking for</p>
-  // <a href="/">back home</a>
-  //   `)
+  if (req.url === '/') {
+    res.end('Welcome to our home page');
+  }
+  if (req.url === '/about') {
+    res.end('Here is our short history');
+  }
+  res.end(`
+    <h1>Oops!</h1>
+  <p>We can't seem to find the page you are looking for</p>
+  <a href="/">back home</a>
+  //   `);
   // ###################################
   // ###################################
   //
@@ -19,16 +19,16 @@ const server = http.createServer((req, res) => {
   // SWITCH TO IF, ELSE IF, ELSE (BELOW)
   // WE COVER THE CAUSE, LATER IN EXPRESS TUTORIAL
   if (req.url === '/') {
-    res.end('Welcome to our home page')
+    res.end('Welcome to our home page');
   } else if (req.url === '/about') {
-    res.end('Here is our short history')
+    res.end('Here is our short history');
   } else {
     res.end(`
     <h1>Oops!</h1>
     <p>We can't seem to find the page you are looking for</p>
     <a href="/">back home</a>
-    `)
+    `);
   }
-})
+});
 
-server.listen(5000)
+server.listen(5000);
