89ed79d42f0f0e1cc11457f7d2422b13841cf1d8
stharavi01 <sanayashe@gmail.com>
2024-03-29 15:20:37 +0545

    http steam

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,8 @@
-console.log('Express Tutorial')
+const http = require('http');
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'content-type': 'text/html' });
+    res.write('<h1>Home Page</h1>');
+    res.end();
+});
+
+server.listen(5000);
