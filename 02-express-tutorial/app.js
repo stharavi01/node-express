3f8d587efef74d61481fb61bab82faa041231daf
stharavi01 <sanayashe@gmail.com>
2024-03-29 22:29:36 +0545

    route parameters

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,18 +1,31 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
 
-app.use(express.static('./public'));
+const { products } = require('./data');
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './navbar-app/index.html'));
+    res.send('<h1> Home Page</h1><a href="/api/products">products</a>');
 });
 
-app.get('*', (req, res) => {
-    res.status(404).send("Resource not found");
+app.get('/api/products', (req, res) => {
+    const newProducts = products.map((product) => {
+        const { id, name, image } = product;
+        return { id, name, image };
+    });
+    res.json(newProducts);
+});
+
+app.get('/api/products/:productID', (req, res) => {
+    const { productID } = req.params;
+
+    const singleProduct = products.find((product) => product.id === Number(productID));
+
+    if (!singleProduct) {
+        res.status(404).send('Product does not exist');
+    }
+    return res.json(singleProduct);
 });
 
 app.listen(5000, () => {
-    console.log('Server is listening on port 5000....');
+    console.log("Server is listening on port 5000...");
 });
