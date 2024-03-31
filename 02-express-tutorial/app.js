fc9c1cc61c4a8febd860b7853d618f467e1c39e4
stharavi01 <sanayashe@gmail.com>
2024-03-31 11:50:49 +0545

    middleware test

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,21 +1,27 @@
 const express = require('express');
 const app = express();
+const logger = require('./logger');
+const authorize = require('./authorize');
 
-const logger = (req, res, next) => {
-    const method = req.method;
-    const url = req.url;
-    const time = new Date().getFullYear();
-    console.log(method, url, time);
-    next();
-};
+//middleware functions
+app.use([logger, authorize]);
 
-app.get('/', (req, logger, res) => {
+app.get('/', (req, res) => {
     res.send("home");
 });
 
-app.get('/about', logger, (req, res) => {
+app.get('/about', (req, res) => {
     res.send("about");
 });
+
+app.get('/api/products', (req, res) => {
+    res.send("Products");
+});
+
+app.get('/api/items', (req, res) => {
+    res.send("Items");
+});
+
 
 app.listen(5000, () => {
     console.log('Server is listening on port 5000....');
--- 02-express-tutorial/final/10-middleware-options.js
+++ 02-express-tutorial/final/10-middleware-options.js
@@ -1,28 +1,30 @@
-const express = require('express')
-const app = express()
-const morgan = require('morgan')
-const logger = require('./logger')
-const authorize = require('./authorize')
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const logger = require('./logger');
+const authorize = require('./authorize');
 //  req => middleware => res
 
 // app.use([logger, authorize])
 // app.use(express.static('./public'))
-app.use(morgan('tiny'))
+
+//middleware functions
+app.use(morgan('tiny'));
 
 app.get('/', (req, res) => {
-  res.send('Home')
-})
+  res.send('Home');
+});
 app.get('/about', (req, res) => {
-  res.send('About')
-})
+  res.send('About');
+});
 app.get('/api/products', (req, res) => {
-  res.send('Products')
-})
+  res.send('Products');
+});
 app.get('/api/items', (req, res) => {
-  console.log(req.user)
-  res.send('Items')
-})
+  console.log(req.user);
+  res.send('Items');
+});
 
 app.listen(5000, () => {
-  console.log('Server is listening on port 5000....')
-})
+  console.log('Server is listening on port 5000....');
+});
