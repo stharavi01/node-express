87e0241981aa7f03cb622ba21f41eb62d3e86ebf
stharavi01 <sanayashe@gmail.com>
2024-04-02 12:55:27 +0545

    basic API structure

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,13 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks');
+
+//middleware
+app.use(express.json());
+
+//routes
+app.use('/api/v1/tasks', tasks);
+
+const port = 3000;
+
+app.listen(port, console.log(`Server is listening on port ${port}...`));
