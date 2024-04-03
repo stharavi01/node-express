8c28ca327e39c217f8cf492f504f6060835b0c77
stharavi01 <sanayashe@gmail.com>
2024-04-03 12:14:44 +0545

    create and get all tasks

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,12 +1,21 @@
 const Task = require('../models/Task');
 
-const getAllTasks = (req, res) => {
-    res.send('get all tasks');
+const getAllTasks = async (req, res) => {
+    try {
+        const tasks = await Task.find({});
+        res.status(200).json({ tasks });
+    } catch (error) {
+        res.status(500).json({ msg: error });
+    }
 };
 
 const createTask = async (req, res) => {
-    const task = await Task.create(req.body);
-    res.status(201).json({ task });
+    try {
+        const task = await Task.create(req.body);
+        res.status(201).json({ task });
+    } catch (error) {
+        res.status(500).json({ msg: error });
+    }
 };
 
 const getTask = (req, res) => {
