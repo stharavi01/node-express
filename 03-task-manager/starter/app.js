d6ac75cd9d0ed195795c6b123bb6b7919b9d5dec
stharavi01 <sanayashe@gmail.com>
2024-04-03 10:09:33 +0545

    setting up env variables

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
+require('dotenv').config();
 
 //middleware
 app.use(express.json());
@@ -13,7 +14,7 @@ const port = 3000;
 
 const start = async () => {
     try {
-        await connectDB();
+        await connectDB(process.env.MONGO_URI);
         app.listen(port, console.log(`Server is listening on port ${port}...`));
     } catch (error) {
         console.log(error);
--- 03-task-manager/starter/db/connect.js
+++ 03-task-manager/starter/db/connect.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://stharavi01:ynvTYg50@nodeexpressprojects.0tjicns.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority&appName=NodeExpressProjects';
-
 const connectDB = (url) => {
-    return mongoose.connect(connectionString);
+    return mongoose.connect(url);
 };
-
 
 module.exports = connectDB;
 
