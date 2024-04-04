2821e1922419651ea0a80d70f3f1cc6adc2674ee
stharavi01 <sanayashe@gmail.com>
2024-04-04 15:56:58 +0545

    sort select limit & skip

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,21 +1,39 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-    const products = await Product.find({
-        name: 'vase table',
-    });
+    const products = await Product.find({}).select('name price').limit(4);
     res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-    const { featured } = req.query;
+    const { featured, company, name, sort, fields } = req.query;
     const queryObject = {};
 
     if (featured) {
         queryObject.featured = featured === 'true' ? true : false;
     }
-    console.log(queryObject);
-    const products = await Product.find(queryObject);
+    if (company) {
+        queryObject.company = company;
+    }
+    if (name) {
+        queryObject.name = { $regex: name, $options: 'i' };
+    }
+    // console.log(queryObject);
+    let result = Product.find(queryObject);
+    // Sort
+    if (sort) {
+        const sortList = sort.split(',').join(' ');
+        result = result.sort(sortList);
+    } else {
+        result = result.sort('createdAt');
+    }
+    //Select
+    if (fields) {
+        const fieldList = fields.split(',').join(' ');
+        result = result.select(fieldList);
+    }
+    const products = await result;
+
     res.status(200).json({ products, nbHits: products.length });
 };
 
