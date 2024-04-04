7e32d15dd2a4151c5528a91ec9eb9e4269d9b2e7
stharavi01 <sanayashe@gmail.com>
2024-04-04 09:26:23 +0545

    model/schema defined

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
-//async errors
+require('express-async-errors');
 
 const express = require('express');
 const app = express();
 
 const connectDB = require('./db/connect');
+
+const productsRouter = require('./routes/products');
 
 const notFoundMiddleware = require('./middleware/not-found');
 const errorMiddleware = require('./middleware/error-handler');
@@ -16,6 +18,8 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('<h1>Store API</h1><a href="/api/v1/products">Products</a>');
 });
+
+app.use('/api/v1/products', productsRouter);
 
 const port = process.env.PORT || 3000;
 
