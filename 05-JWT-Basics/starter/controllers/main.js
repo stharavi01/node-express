af4d525010a234d560760da0932e855204c02ffe
stharavi01 <sanayashe@gmail.com>
2024-04-06 22:06:51 +0545

    JWT implementation

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,11 +1,20 @@
 const CustomAPIError = require('../errors/custom-error');
+const jwt = require('jsonwebtoken');
 
 const login = async (req, res) => {
     const { username, password } = req.body;
     if (!username || !password) {
         throw new CustomAPIError("Please provide email and password", 400);
     }
-    res.send("Fake Login/Register/Signup Route");
+
+    //just for demo, normally provided by DB!!
+    const id = new Date().getDate();
+
+    //try to keep payload small, better experience for user
+    //just for demo, in production use long, complex and unguessable string value!!!
+    const token = jwt.sign({ id, username }, process.env.JWT_SECRET);
+
+    res.status(200).json({ msg: "user created", token });
 };
 
 const dashboard = async (req, res) => {
