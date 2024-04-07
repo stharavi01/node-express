0498f82edceab14ea173ed284004cbf2ac34f03b
stharavi01 <sanayashe@gmail.com>
2024-04-07 19:33:35 +0545

    set up controller & router

--- 06-jobs-api/starter/app.js
+++ 06-jobs-api/starter/app.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
 const app = express();
+const authRouter = require('./routes/auth');
+const jobsRouter = require('./routes/jobs');
 
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
@@ -10,10 +12,13 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 app.use(express.json());
 // extra packages
 
-// routes
-app.get('/', (req, res) => {
-  res.send('jobs api');
-});
+// demo routes
+// app.get('/', (req, res) => {
+//   res.send('jobs api');
+// });
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
