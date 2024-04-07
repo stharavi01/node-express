ca9b3f4c9d756a35e015fa6cfdc48064b500c833
stharavi01 <sanayashe@gmail.com>
2024-04-07 12:35:18 +0545

    add custom error classes

---
+++ 05-JWT-Basics/starter/errors/bad-request.js
@@ -0,0 +1,12 @@
+const CustomAPIError = require("./custom-error");
+//named import
+const { StatusCodes } = require('http-status-codes');
+
+class BadRequestError extends CustomAPIError {
+    constructor(message, statusCode) {
+        super(message);
+        this.statusCode = StatusCodes.BAD_REQUEST;
+    }
+}
+
+module.exports = BadRequestError;
