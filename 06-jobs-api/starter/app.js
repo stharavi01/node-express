d2c450ea72feb9c763dc44ebab449fb8132ed775
stharavi01 <sanayashe@gmail.com>
2024-04-07 19:57:13 +0545

    connect to mongoDB

--- 06-jobs-api/starter/app.js
+++ 06-jobs-api/starter/app.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
 const app = express();
+
+const connectDB = require('./db/connect');
+
 const authRouter = require('./routes/auth');
 const jobsRouter = require('./routes/jobs');
 
@@ -27,6 +30,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
